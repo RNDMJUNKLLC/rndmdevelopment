7c95ab11aac71f0418f6d9e927020576007e6ac7
RNDMJUNKLLC <william@rndmdevs.com>
2026-01-26 08:46:37 +0000

    🎯 Add token.html to Rollup input configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
         about: resolve(__dirname, 'about.html'),
         services: resolve(__dirname, 'services.html'),
         contact: resolve(__dirname, 'contact.html'),
+        token: resolve(__dirname, 'token.html'),
       },
     },
   },
